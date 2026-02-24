b53fd77d91102fa43bd0d61e563cc3e81b94da58
laxmesree-netizen <laxmesree@gmail.com>
2026-02-24 16:36:26 +0530

    BranchB_work

--- src/org/zoho/samplegitclass.java
+++ src/org/zoho/samplegitclass.java
@@ -8,6 +8,15 @@ public static  void gitData1() {
 public static void main(String[] args) {
 	gitData1();
 	int a1 = 4443;
-	System.out.println(a1);
+
+	long b = 7859632;
+	
+	if (b==523) {
+		System.out.println("condition True");
+	}
+	else {
+		System.out.println("Condition False");
+	}
+	System.out.println("BranchB work done");
 }
 }
