c5115ba47509b8dfb9d4492d8b05685cc3919d2f
laxmesree-netizen <laxmesree@gmail.com>
2026-02-24 14:48:05 +0530

    done

---
+++ src/org/zoho/samplegitclass.java
@@ -0,0 +1,13 @@
+package org.zoho;
+
+public class samplegitclass {
+public static  void gitData1() {
+	
+
+}
+public static void main(String[] args) {
+	gitData1();
+	int a1 = 4443;
+	System.out.println(a1);
+}
+}
