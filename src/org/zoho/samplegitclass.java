c3fbd195eb4d89330eb4f162745030041231ca92
laxmesree-netizen <laxmesree@gmail.com>
2026-02-24 15:57:24 +0530

    branchAwork Done

--- src/org/zoho/samplegitclass.java
+++ src/org/zoho/samplegitclass.java
@@ -2,12 +2,15 @@ package org.zoho;
 
 public class samplegitclass {
 public static  void gitData1() {
-	
+	System.out.println("Git Data1");
 
 }
 public static void main(String[] args) {
 	gitData1();
 	int a1 = 4443;
 	System.out.println(a1);
+	System.out.println("BranchA start the work");
+	String data = "Test Data Verified";
+	System.out.println(data);
 }
 }
